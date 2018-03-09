8995e0122792a00f38853e3a9de77ea33e1683d8
Kamil Machański <>
2018-03-09 14:29:46 +0100

    menu wyboru

--- POLab2/src/pl/edu/ur/main/Main.java
+++ POLab2/src/pl/edu/ur/main/Main.java
@@ -1,16 +1,46 @@
 package pl.edu.ur.main;
 
-/**
- *
- * @author marcin
- */
+import java.util.Scanner;
+
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO zad 3, 4, 6, 7
+        
+        
+        Scanner input = new Scanner(System.in);
+
+        MojeZadania z1 = new MojeZadania();
+
+        int a = 0;
+
+        while (a == 0) {
+
+            System.out.println("Podaj nr zadania: (od 1 do 7)(0 aby wyjść)");
+            int n = input.nextInt();
+
+            if (n == 1) {
+                z1.zadanie1();
+            } else if (n == 2) {
+                z1.zadanie2();
+            } else if (n == 3) {
+                z1.zadanie3();
+            } else if (n == 4) {
+                z1.zadanie4();
+            } else if (n == 5) {
+                z1.zadanie5();
+            } else if (n == 6) {
+                z1.zadanie6();
+            } else if (n == 7) {
+                z1.zadanie7();
+            } else if (n == 0) {
+                a = 1;
+            } else {
+                System.out.println("Zły wybór !!!");
+            }
+        }
     }
     
 }
